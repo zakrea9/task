8d2d17f4ba98c599e6c6a0a02dd7193634c13b87
zakrea emad <zakreaemad999@gmail.com>
2022-07-26 10:29:40 +0300

    update

--- corn.php
+++ corn.php
@@ -3,18 +3,25 @@ include('C:\xampp\htdocs\index\index.php');
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname="mydatabase";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password,$dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $name=$_POST['user'];
     $password=$_POST['pass'];
-    echo $name . $password;
+    
+    $sql = "INSERT INTO users ( zakrea , password)
+    VALUES ('$name', '$password')";
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
 }
